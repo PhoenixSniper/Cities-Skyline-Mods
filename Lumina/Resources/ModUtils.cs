a6975176b99e665bde6ac881102a30106980bc09
PhoenixSniper <gamerzyt1699@gmail.com>
2025-12-14 13:59:26 +0530

    first commit

---
+++ Lumina/Resources/ModUtils.cs
@@ -0,0 +1,45 @@
+﻿using ColossalFramework.Plugins;
+using System.Reflection;
+using Lumina;
+
+namespace Lumina
+{
+    public static class ModUtils
+    {
+     
+        
+        public static bool IsAnyModsEnabled(string[] names)
+        {
+            foreach (string name in names)
+            {
+                if (IsModEnabled(name))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Helper method to check for enabled mods.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static bool IsModEnabled(string name)
+        {
+            foreach (PluginManager.PluginInfo plugin in PluginManager.instance.GetPluginsInfo())
+            {
+                foreach (Assembly assembly in plugin.GetAssemblies())
+                {
+                    if (assembly.GetName().Name.ToLower() == name.ToLower())
+                    {
+                        return plugin.isEnabled;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
